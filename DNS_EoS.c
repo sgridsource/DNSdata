6ac10375d9cbb04b5981a15e8424f492833ee2e4
Wolfgang <wolf@fau.edu>
2020-02-28 17:44:05 -0500

    add DNS_EoS.c with genral EoS funcs

---
+++ DNS_EoS.c
@@ -0,0 +1,23 @@
+/* genral EoS funcs */
+/* Wolfgang Tichy, 2/2020 */
+
+#include "sgrid.h"
+#include "DNSdata.h"
+
+
+/**************************************************************************/
+/* some general functions in that hold for any EoS */
+/**************************************************************************/
+
+/* return h-1 = epsl + P/rho0 */
+double hm1_of_rho0_epsl_P(double rho0, double epsl, double P)
+{
+  if(rho0==0.) return 0.;
+  else         return epsl + P/rho0;
+}
+
+/* return h = 1 + epsl + P/rho0 */
+double h_of_rho0_epsl_P(double rho0, double epsl, double P)
+{
+  return hm1_of_rho0_epsl_P(rho0, epsl, P) + 1.;
+}
