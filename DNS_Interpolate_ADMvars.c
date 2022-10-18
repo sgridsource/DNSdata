ddbfed5d2faceab0c8ca72de177ecfc203247223
Wolfgang <wolf@fau.edu>
2022-10-18 10:33:20 -0400

    no interp if BNSdata_Interpolate_pointsfile=****NONE****

--- DNS_Interpolate_ADMvars.c
+++ DNS_Interpolate_ADMvars.c
@@ -41,7 +41,7 @@ int read_next_xyz_from_pointsfile(FILE *in, double *x, double *y, double *z)
    BNSdata_Interpolate_verbose = no # yes
    BNSdata_Interpolate_max_xyz_diff = 1e-8
    The last 2 are just for debugging. Their values are set e.g. by BAM.
-   We do nothing if BNSdata_Interpolate_pointsfile = *** NONE ***        */
+   We do nothing if BNSdata_Interpolate_pointsfile = ****NONE****        */
 int DNS_Interpolate_ADMvars(tGrid *grid)
 {
   int pr = GetvLax("BNSdata_Interpolate_verbose", "yes");
@@ -59,7 +59,7 @@ int DNS_Interpolate_ADMvars(tGrid *grid)
   int b, j;
 
   if(GetsLax("BNSdata_Interpolate_pointsfile")==0) return 0;
-  if(strcmp(Gets("BNSdata_Interpolate_pointsfile"), "*** NONE ***")==0)
+  if(strcmp(Gets("BNSdata_Interpolate_pointsfile"), "****NONE****")==0)
     return 0;
   prdivider(0);
   printf("DNS_Interpolate_ADMvars:\n");
